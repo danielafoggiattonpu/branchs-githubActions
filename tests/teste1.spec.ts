200629905ac7de68620efacad8dba3a5a5835a8b
danielafoggiattonpu <daniela.foggiatto@npu.com.br>
2025-10-31 10:32:50 -0300

    dani4

--- tests/teste1.spec.ts
+++ tests/teste1.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test.only('Página inicial abre corretamente', async ({ page }) => {
+test('Página inicial abre corretamente', async ({ page }) => {
   await page.goto('https://example.com'); // você pode colocar qualquer URL de teste
   await expect(page).toHaveTitle(/Example Domain/);
 });
